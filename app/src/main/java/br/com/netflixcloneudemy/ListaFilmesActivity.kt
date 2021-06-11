137c5b333e98df94225f9be38cd56ba808a0ff14
Ana Clara Guerra <ana.guerrads@hotmail.com>
2021-06-10 22:20:08 -0300

    Firebase Desloga Usuario

--- app/src/main/java/br/com/netflixcloneudemy/ListaFilmesActivity.kt
+++ app/src/main/java/br/com/netflixcloneudemy/ListaFilmesActivity.kt
@@ -1,8 +1,12 @@
 package br.com.netflixcloneudemy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import br.com.netflixcloneudemy.databinding.ActivityListaFilmesBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class ListaFilmesActivity : AppCompatActivity() {
 
@@ -13,6 +17,29 @@ class ListaFilmesActivity : AppCompatActivity() {
         binding = ActivityListaFilmesBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val inflateMenu = menuInflater
+        inflateMenu.inflate(R.menu.lista_filmes_menu, menu)
+        return true
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.menu_netflix_deslogar -> {
+                FirebaseAuth.getInstance().signOut()
+                voltaParaFormLogin()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun voltaParaFormLogin() {
+        val intent = Intent(this, FormLogin::class.java)
+        startActivity(intent)
+        finish()
     }
 }
